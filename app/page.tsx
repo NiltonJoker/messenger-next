4298377c08468722c8f4a4ab147703dee6e26b55
Nilton <nilton242009@hotmail.com>
2022-12-17 17:12:47 -0500

    SSR and loading messages

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
+import { Message } from "../typings";
+import ChatInput from "./ChatInput";
+import MessageList from "./MessageList";
 
-import ChatInput from './ChatInput'
-import MessageList from './MessageList'
+async function HomePage() {
+  const data = await fetch(`${process.env.VERCEL_URL || 'http://localhost:3000'}/api/getMessages`).then(
+    (res) => res.json()
+  );
 
-function HomePage() {
+  const messages: Message[] = data.messages
   return (
     <main>
       {/* MessageList */}
-      <MessageList/>
+      <MessageList initialMessages={messages} />
       {/* ChatInput */}
-      <ChatInput/>
+      <ChatInput />
     </main>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
